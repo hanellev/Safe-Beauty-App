aba83dbfc554a563d2e0b22f6f03e4811bee418e
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-01 00:00:38 +0100

    Importing React Router, creating nav and links, styling nav in css, creating page Ingredients List.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,49 @@
 import { createRoot } from 'react-dom/client';
 import { HomePage } from './pages/HomePage';
+import * as React from 'react';
+import { IngredientsList } from './pages/IngredientsList/IngredientsList';
 import './global.css';
+import {
+	createBrowserRouter,
+	RouterProvider,
+	Outlet,
+	Link,
+	NavLink,
+} from 'react-router-dom';
 
-createRoot(document.querySelector('#app')).render(<HomePage />);
+const App = () => {
+	return (
+		<div className="container">
+			<nav>
+				<NavLink to="/">Home Page</NavLink>
+				<span> | </span>
+				<NavLink to="/Ingredients List">Ingredients List</NavLink>
+				<span> | </span>
+			</nav>
+			<Outlet />
+		</div>
+	);
+};
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		// errorElement: <ErrorPage />,
+
+		children: [
+			{
+				path: '/',
+				element: <HomePage />,
+			},
+			{
+				path: 'Ingredients List',
+				element: <IngredientsList />,
+			},
+		],
+	},
+]);
+
+createRoot(document.querySelector('#app')).render(
+	<RouterProvider router={router} />
+);
