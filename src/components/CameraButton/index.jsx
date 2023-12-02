7615443f16fc636098555c28c5c7990d1bb78c05
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-02 18:21:34 +0100

    Change the button text to 'Use a camera' when camera is not opened

--- src/components/CameraButton/index.jsx
+++ src/components/CameraButton/index.jsx
@@ -1,58 +1,115 @@
-import "./index.css";
-import React, { useState } from "react";
-import Webcam from "react-webcam";
+import './index.css';
+import React, { useState } from 'react';
+import Webcam from 'react-webcam';
+
+// const CameraButton = () => {
+//   const [picture, setPicture] = useState(null);
+//   const [isCameraOpen, setIsCameraOpen] = useState(false);
+
+//   const webcamRef = React.useRef(null);
+
+//   const capture = React.useCallback(() => {
+//     if (webcamRef.current) {
+//       const pictureSrc = webcamRef.current.getScreenshot();
+//       setPicture(pictureSrc);
+//     }
+//   }, [webcamRef, setPicture]);
+
+//   const handleChange = (e) => {
+//     setPicture(URL.createObjectURL(e.target.files[0]));
+//   };
+
+//   const handleClick = () => {
+//     if (!isCameraOpen) {
+//       setIsCameraOpen(true);
+//       return;
+//     }
+
+//     if (isCameraOpen) {
+//       capture();
+//       setIsCameraOpen(false);
+//       // TODO: implement logic to send picture to api
+//     }
+//   };
+
+//   if (picture) {
+//     return <span>loading...</span>;
+//   }
+
+//   return (
+//     <div className="container">
+//       {isCameraOpen && (
+//         <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+//       )}
+//       {picture ? (
+//         <img src={picture} />
+//       ) : (
+//         <>
+//           <button className="take-a-pict" onClick={handleClick}>
+//             TAKE PICTURE
+//           </button>
+//           <input type="file" onChange={handleChange} className="upload" />
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default CameraButton;
 
 const CameraButton = () => {
-  const [picture, setPicture] = useState(null);
-  const [isCameraOpen, setIsCameraOpen] = useState(false);
+	const [picture, setPicture] = useState(null);
+	const [isCameraOpen, setIsCameraOpen] = useState(false);
 
-  const webcamRef = React.useRef(null);
+	const webcamRef = React.useRef(null);
 
-  const capture = React.useCallback(() => {
-    if (webcamRef.current) {
-      const pictureSrc = webcamRef.current.getScreenshot();
-      setPicture(pictureSrc);
-    }
-  }, [webcamRef, setPicture]);
+	const capture = React.useCallback(() => {
+		if (webcamRef.current) {
+			const pictureSrc = webcamRef.current.getScreenshot();
+			setPicture(pictureSrc);
+		}
+	}, [webcamRef, setPicture]);
 
-  const handleChange = (e) => {
-    setPicture(URL.createObjectURL(e.target.files[0]));
-  };
+	const handleChange = (e) => {
+		setPicture(URL.createObjectURL(e.target.files[0]));
+	};
 
-  const handleClick = () => {
-    if (!isCameraOpen) {
-      setIsCameraOpen(true);
-      return;
-    }
+	const buttonText = isCameraOpen ? 'TAKE A PICTURE' : 'USE A CAMERA';
 
-    if (isCameraOpen) {
-      capture();
-      setIsCameraOpen(false);
-      // TODO: implement logic to send picture to api
-    }
-  };
+	const handleClick = () => {
+		if (!isCameraOpen) {
+			setIsCameraOpen(true);
+			return;
+		}
 
-  if (picture) {
-    return <span>loading...</span>;
-  }
+		if (isCameraOpen) {
+			capture();
+			setIsCameraOpen(false);
+			// TODO: implement logic to send picture to api
+		}
+	};
 
-  return (
-    <div className="container">
-      {isCameraOpen && (
-        <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
-      )}
-      {picture ? (
-        <img src={picture} />
-      ) : (
-        <>
-          <button className="take-a-pict" onClick={handleClick}>
-            TAKE PICTURE
-          </button>
-          <input type="file" onChange={handleChange} className="upload" />
-        </>
-      )}
-    </div>
-  );
+	if (picture) {
+		return <span>loading...</span>;
+	}
+
+	return (
+		<div className="container">
+			{isCameraOpen && (
+				<Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+			)}
+			{picture ? (
+				<img src={picture} alt="Captured" />
+			) : (
+				<>
+					<button className="take-a-pict" onClick={handleClick}>
+						{buttonText}
+					</button>
+					<input type="file" onChange={handleChange} className="upload" />
+				</>
+			)}
+		</div>
+	);
 };
 
 export default CameraButton;
