b987795b1f4db3a229314525fd1308571ca0dc09
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-03 17:19:17 +0100

    Adding use Effect to redirect the user from Home Page to Ingredients List

--- src/components/CameraButton/index.jsx
+++ src/components/CameraButton/index.jsx
@@ -1,9 +1,185 @@
 import './index.css';
 import React, { useState, useEffect } from 'react';
 import { processPhotoEden } from '../../api/edenAi';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+// import Webcam from 'react-webcam';
 
+// const CameraButton = () => {
+//   const [picture, setPicture] = useState(null);
+//   const [isCameraOpen, setIsCameraOpen] = useState(false);
+
+//   const webcamRef = React.useRef(null);
+
+//   const capture = React.useCallback(() => {
+//     if (webcamRef.current) {
+//       const pictureSrc = webcamRef.current.getScreenshot();
+//       setPicture(pictureSrc);
+//     }
+//   }, [webcamRef, setPicture]);
+
+//   const handleChange = (e) => {
+//     setPicture(URL.createObjectURL(e.target.files[0]));
+//   };
+
+//   const handleClick = () => {
+//     if (!isCameraOpen) {
+//       setIsCameraOpen(true);
+//       return;
+//     }
+
+//     if (isCameraOpen) {
+//       capture();
+//       setIsCameraOpen(false);
+//       // TODO: implement logic to send picture to api
+//     }
+//   };
+
+//   if (picture) {
+//     return <span>loading...</span>;
+//   }
+
+//   return (
+//     <div className="container">
+//       {isCameraOpen && (
+//         <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+//       )}
+//       {picture ? (
+//         <img src={picture} />
+//       ) : (
+//         <>
+//           <button className="take-a-pict" onClick={handleClick}>
+//             TAKE PICTURE
+//           </button>
+//           <input type="file" onChange={handleChange} className="upload" />
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default CameraButton;
+
+// const CameraButton = () => {
+// 	const [picture, setPicture] = useState(null);
+// 	const [isCameraOpen, setIsCameraOpen] = useState(false);
+
+// 	const webcamRef = React.useRef(null);
+
+// 	const capture = React.useCallback(() => {
+// 		if (webcamRef.current) {
+// 			const pictureSrc = webcamRef.current.getScreenshot();
+// 			setPicture(pictureSrc);
+// 		}
+// 	}, [webcamRef, setPicture]);
+
+// 	const handleChange = (e) => {
+// 		setPicture(URL.createObjectURL(e.target.files[0]));
+// 	};
+
+// 	const buttonText = isCameraOpen ? 'Take a picture' : 'Use a camera';
+
+// 	const handleClick = () => {
+// 		if (!isCameraOpen) {
+// 			setIsCameraOpen(true);
+// 			return;
+// 		}
+
+// 		if (isCameraOpen) {
+// 			capture();
+// 			setIsCameraOpen(false);
+// 			// TODO: implement logic to send picture to api
+// 		}
+// 	};
+
+// 	if (picture) {
+// 		return <span>loading...</span>;
+// 	}
+
+// 	return (
+// 		<div className="button-container">
+// 		{isCameraOpen && (
+// 				<Webcam
+// 					className="webcam-window"
+// 					audio={false}
+// 					ref={webcamRef}
+// 					screenshotFormat="image/jpeg"
+// 					videoConstraints={{
+// 						facingMode: 'environment',
+// 					}}
+// 				/>
+// 			)}
+// 			{picture ? (
+// 				<img src={picture} alt="Captured" />
+// 			) : (
+// 				<>
+// 					<button className="take-a-pict" onClick={handleClick}>
+// 						{buttonText}
+// 					</button> */
+// 			<input
+// 				type="file"
+// 				onChange={handleChange}
+// 				className="upload"
+// 				style={{ display: 'none' }}
+// 			/>
+// 			<button
+// 				className="upload-button"
+// 				onClick={() => document.querySelector('.upload').click()}
+// 			>
+// 				Upload Picture
+// 			</button>
+// 		</div>
+// 	);
+// };
+
+// export default CameraButton;
+
+// const CameraButton = () => {
+// 	const [picture, setPicture] = useState(null);
+
+// 	const handleChange = (e) => {
+// 		setPicture(URL.createObjectURL(e.target.files[0]));
+// 	};
+
+// 	useEffect(() => {
+// 		if (picture) {
+// 			return <span>loading...</span>;
+// 		}
+
+// 		picture &&
+// 			setTimeout(() => {
+// 				redirect('/Ingredients List');
+// 			}, 3000);
+// 		return () => {
+// 			console.log('cleanup');
+// 		};
+// 	}, [redirect, picture]);
+
+// 	return (
+// 		<div className="button-container">
+// 			<input
+// 				type="file"
+// 				onChange={handleChange}
+// 				className="upload"
+// 				style={{ display: 'none' }}
+// 			/>
+// 			<button className="take-a-pict"></button>
+
+// 			<button
+// 				className="upload-button"
+// 				onClick={() => document.querySelector('.upload').click()}
+// 			>
+// 				Select image
+// 			</button>
+// 		</div>
+// 	);
+// };
+
+// export default CameraButton;
 const CameraButton = () => {
-  const [picture, setPicture] = useState(null);
+	const [picture, setPicture] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [redirect, setRedirect] = useState(false);
 
   const handleChange = (e) => {
     setPicture(URL.createObjectURL(e.target.files[0]));
@@ -14,41 +190,49 @@ const CameraButton = () => {
     }
   }, [picture]);
 
-  const handleClick = () => {
-    if (!isCameraOpen) {
-      setIsCameraOpen(true);
-      return;
-    }
+	useEffect(() => {
+		let timer;
 
-    if (isCameraOpen) {
-      capture();
-      setIsCameraOpen(false);
-      // TODO: implement logic to send picture to api
-    }
-  };
+		if (picture) {
+			setLoading(true);
 
-  if (picture) {
-    return <span>loading...</span>;
-  }
+			timer = setTimeout(() => {
+				setRedirect(true);
+			}, 5000);
+		}
 
-  return (
-    <div className="button-container">
-      <input
-        type="file"
-        onChange={handleChange}
-        className="upload"
-        style={{ display: 'none' }}
-      />
-      <button className="take-a-pict"></button>
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [picture]);
 
-      <button
-        className="upload-button"
-        onClick={() => document.querySelector('.upload').click()}
-      >
-        Select image
-      </button>
-    </div>
-  );
+	if (redirect) {
+		return <Redirect to="/Ingredients List" />;
+	}
+
+	return (
+		<div className="button-container">
+			{loading ? (
+				<span>Loading...</span>
+			) : (
+				<>
+					<input
+						type="file"
+						onChange={handleChange}
+						className="upload"
+						style={{ display: 'none' }}
+					/>
+					<button className="take-a-pict"></button>
+					<button
+						className="upload-button"
+						onClick={() => document.querySelector('.upload').click()}
+					>
+						Select image
+					</button>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default CameraButton;
