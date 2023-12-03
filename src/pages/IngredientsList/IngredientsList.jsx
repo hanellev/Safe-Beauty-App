1299904db604642ba9ab71610e5f69e562e84ca2
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-03 16:28:50 +0100

    Content of Ingredients List page

--- src/pages/IngredientsList/IngredientsList.jsx
+++ src/pages/IngredientsList/IngredientsList.jsx
@@ -3,16 +3,45 @@ import './IngredientsList.css';
 export const IngredientsList = () => {
 	return (
 		<div className="container">
-			<h2>Lorem ipsum</h2>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.
-			</p>
+			<h2>Ingredients List</h2>
+
+			<div className="ingredient">
+				<div className="ingredient__safe">
+					<img
+						className="ingredient__harmful-icon"
+						src="./images/cream-green.png"
+						alt="Safe Icon"
+					/>
+					<div className="ingredient__text">
+						<h3>Safe Ingredients</h3>
+						<p>Amount:</p>
+					</div>
+				</div>
+
+				<div className="ingredient__harmful">
+					<img
+						className="ingredient__harmful-icon"
+						src="./images/cream-orange.png"
+						alt="Harmful Icon"
+					/>
+					<div className="ingredient__text">
+						<h3>Harmful Ingredients</h3>
+						<p>Amount:</p>
+					</div>
+				</div>
+
+				<div className="ingredient__dangerous">
+					<img
+						className="ingredient__harmful-icon"
+						src="./images/cream-red.png"
+						alt="Dangerous Icon"
+					/>
+					<div className="ingredient__text">
+						<h3>Dangerous Ingredients</h3>
+						<p>Amount:</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
