1b970833914835064d3fe95d26a87e82e5ca1237
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-03 11:55:20 +0100

    Modification of Camre Button code for mobile phone testing

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,14 +1,21 @@
 import './style.css';
 import { Header } from '/components/Header/Header.jsx';
 import { Footer } from '/components/Footer/Footer.jsx';
-import CameraButton from '../../components/CameraButton';
+// import CameraButton from '../../components/CameraButton';
 
 export const HomePage = () => {
-  return (
-    <div className="container">
-      <Header />
-      <CameraButton />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="container">
+			<Header />
+			{/* <CameraButton /> */}
+			<button
+				className="upload-button"
+				onClick={() => document.querySelector('.upload').click()}
+			>
+				Upload Picture
+			</button>
+			;
+			<Footer />
+		</div>
+	);
 };
