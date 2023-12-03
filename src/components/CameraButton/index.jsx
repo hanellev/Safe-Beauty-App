6618f69b58cfdb0c79233a3f9df4dbf735bf24d0
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-03 12:31:03 +0100

    Modificaton of code for mobile phone testing

--- src/components/CameraButton/index.jsx
+++ src/components/CameraButton/index.jsx
@@ -1,6 +1,6 @@
 import './index.css';
 import React, { useState } from 'react';
-// import Webcam from 'react-webcam';
+import Webcam from 'react-webcam';
 
 // const CameraButton = () => {
 //   const [picture, setPicture] = useState(null);
@@ -57,37 +57,86 @@ import React, { useState } from 'react';
 
 // export default CameraButton;
 
+// const CameraButton = () => {
+// 	const [picture, setPicture] = useState(null);
+// 	const [isCameraOpen, setIsCameraOpen] = useState(false);
+
+// 	const webcamRef = React.useRef(null);
+
+// 	const capture = React.useCallback(() => {
+// 		if (webcamRef.current) {
+// 			const pictureSrc = webcamRef.current.getScreenshot();
+// 			setPicture(pictureSrc);
+// 		}
+// 	}, [webcamRef, setPicture]);
+
+// 	const handleChange = (e) => {
+// 		setPicture(URL.createObjectURL(e.target.files[0]));
+// 	};
+
+// 	const buttonText = isCameraOpen ? 'Take a picture' : 'Use a camera';
+
+// 	const handleClick = () => {
+// 		if (!isCameraOpen) {
+// 			setIsCameraOpen(true);
+// 			return;
+// 		}
+
+// 		if (isCameraOpen) {
+// 			capture();
+// 			setIsCameraOpen(false);
+// 			// TODO: implement logic to send picture to api
+// 		}
+// 	};
+
+// 	if (picture) {
+// 		return <span>loading...</span>;
+// 	}
+
+// 	return (
+// 		<div className="button-container">
+// 		{isCameraOpen && (
+// 				<Webcam
+// 					className="webcam-window"
+// 					audio={false}
+// 					ref={webcamRef}
+// 					screenshotFormat="image/jpeg"
+// 					videoConstraints={{
+// 						facingMode: 'environment',
+// 					}}
+// 				/>
+// 			)}
+// 			{picture ? (
+// 				<img src={picture} alt="Captured" />
+// 			) : (
+// 				<>
+// 					<button className="take-a-pict" onClick={handleClick}>
+// 						{buttonText}
+// 					</button> */
+// 			<input
+// 				type="file"
+// 				onChange={handleChange}
+// 				className="upload"
+// 				style={{ display: 'none' }}
+// 			/>
+// 			<button
+// 				className="upload-button"
+// 				onClick={() => document.querySelector('.upload').click()}
+// 			>
+// 				Upload Picture
+// 			</button>
+// 		</div>
+// 	);
+// };
+
+// export default CameraButton;
+
 const CameraButton = () => {
 	const [picture, setPicture] = useState(null);
-	// const [isCameraOpen, setIsCameraOpen] = useState(false);
-
-	// const webcamRef = React.useRef(null);
-
-	// const capture = React.useCallback(() => {
-	// 	if (webcamRef.current) {
-	// 		const pictureSrc = webcamRef.current.getScreenshot();
-	// 		setPicture(pictureSrc);
-	// 	}
-	// }, [webcamRef, setPicture]);
 
 	const handleChange = (e) => {
 		setPicture(URL.createObjectURL(e.target.files[0]));
 	};
-
-	// const buttonText = isCameraOpen ? 'Take a picture' : 'Use a camera';
-
-	// const handleClick = () => {
-	// 	if (!isCameraOpen) {
-	// 		setIsCameraOpen(true);
-	// 		return;
-	// 	}
-
-	// 	if (isCameraOpen) {
-	// 		capture();
-	// 		setIsCameraOpen(false);
-	// 		// TODO: implement logic to send picture to api
-	// 	}
-	// };
 
 	if (picture) {
 		return <span>loading...</span>;
@@ -95,24 +144,6 @@ const CameraButton = () => {
 
 	return (
 		<div className="button-container">
-			{/* {isCameraOpen && (
-				<Webcam
-					className="webcam-window"
-					audio={false}
-					ref={webcamRef}
-					screenshotFormat="image/jpeg"
-					videoConstraints={{
-						facingMode: 'environment',
-					}}
-				/>
-			)}
-			{picture ? (
-				<img src={picture} alt="Captured" />
-			) : (
-				<>
-					<button className="take-a-pict" onClick={handleClick}>
-						{buttonText}
-					</button> */}
 			<input
 				type="file"
 				onChange={handleChange}
