a0728cd3bedd477fb851e11a75d8a5881039108b
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-04 18:39:34 +0100

    Error Page basic structure + Error Element in React Router.

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <App />,
-		// errorElement: <ErrorPage />,
+		errorElement: <ErrorPage />,
 
 		children: [
 			{
---
+++ src/pages/Error Page/ErrorPage.jsx
@@ -0,0 +1,9 @@
+export const ErrorPage = () => {
+	return (
+		<main>
+			<h2>404</h2>
+			<p>This is not the page you are looking for.</p>
+			<p>Please, go bact to the home page.</p>
+		</main>
+	);
+};
