a02117aca4fe89de8eb4063244492703327d8f0b
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-04 20:53:18 +0100

    Changed path to the banner to display it

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,10 +3,10 @@ import './ErrorPage.css';
 export const ErrorPage = () => {
 	return (
 		<main>
-			<span role="img" aria-label="banner" className="banner" />
+			<span role="img" aria-label="banner" className="error-banner" />
 			<h2>404</h2>
 			<p>This is not the page you are looking for.</p>
-			<p>Please, go bact to the home page.</p>
+			<p>Please, go back to the home page.</p>
 		</main>
 	);
 };
