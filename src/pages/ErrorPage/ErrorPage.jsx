8b77a8fa96091b03c9510403b60234ecc33cc661
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-04 22:55:50 +0100

    Styling and responsivity of Error Page lamost done.

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -2,11 +2,14 @@ import './ErrorPage.css';
 
 export const ErrorPage = () => {
 	return (
-		<main>
+		<div className="error-container">
 			<span role="img" aria-label="banner" className="error-banner" />
-			<h2>404</h2>
-			<p>This is not the page you are looking for.</p>
-			<p>Please, go back to the home page.</p>
-		</main>
+			<div className="error-text">
+				<h2 id="error-heading">404</h2>
+				<p>This is not the page you are looking for.</p>
+				<p>Please, go back to the home page.</p>
+				<p id="error-text__mobile">Ups...</p>
+			</div>
+		</div>
 	);
 };
