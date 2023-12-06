3bd3d585538d0a6ecec3b405d06d83aab1b84496
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-06 15:54:26 +0100

    Styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,12 @@ import './Footer.css';
 
 export const Footer = () => {
 	return (
-		<>
+		<div className="footer-container">
 			<footer>
 				<p className="footer-text">
 					Final project, Czechitas Digital Academy-Web, autumn 2023
 				</p>
 			</footer>
-		</>
+		</div>
 	);
 };
--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -10,49 +10,49 @@ export const DetailPage = () => {
 			/>
 			<h2>Dangerous Ingredients</h2>
 			<ul>
-				<p>para-dioxane</p>
+				<p className="ingredient-name">para-dioxane</p>
 				<li>Allergenic:</li>
 				<li>Carcinogenic:</li>
 				<li>Suitable for pregnant woman:</li>
 				<li>Impact:</li>
 			</ul>
 			<ul>
-				<p>diethylendioxid</p>
+				<p className="ingredient-name">diethylendioxid</p>
 				<li>Allergenic:</li>
 				<li>Carcinogenic:</li>
 				<li>Suitable for pregnant woman:</li>
 				<li>Impact:</li>
 			</ul>
 			<ul>
-				<p>1,4-DIOXANE</p>
+				<p className="ingredient-name">1,4-DIOXANE</p>
 				<li>Allergenic:</li>
 				<li>Carcinogenic:</li>
 				<li>Suitable for pregnant woman:</li>
 				<li>Impact:</li>
 			</ul>
 			<ul>
-				<p>2-BROM-2-NITROPROPAN-1,3-DIOL</p>
+				<p className="ingredient-name">2-BROM-2-NITROPROPAN-1,3-DIOL</p>
 				<li>Allergenic:</li>
 				<li>Carcinogenic:</li>
 				<li>Suitable for pregnant woman:</li>
 				<li>Impact:</li>
 			</ul>
 			<ul>
-				<p>bronopol</p>
+				<p className="ingredient-name">bronopol</p>
 				<li>Allergenic:</li>
 				<li>Carcinogenic:</li>
 				<li>Suitable for pregnant woman:</li>
 				<li>Impact:</li>
 			</ul>
 			<ul>
-				<p>2-butoxyethanol</p>
+				<p className="ingredient-name">2-butoxyethanol</p>
 				<li>Allergenic:</li>
 				<li>Carcinogenic:</li>
 				<li>Suitable for pregnant woman:</li>
 				<li>Impact:</li>
 			</ul>
 			<ul>
-				<p>ethyleneglycol</p>
+				<p className="ingredient-name">ethyleneglycol</p>
 				<li>Allergenic:</li>
 				<li>Carcinogenic:</li>
 				<li>Suitable for pregnant woman:</li>
