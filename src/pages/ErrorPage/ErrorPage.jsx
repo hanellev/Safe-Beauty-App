2485c3fa77ca2c8cce4dde22b781e42f4e61421c
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-12-10 21:44:51 +0100

    redesign of Error Page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -2,23 +2,23 @@ import './ErrorPage.css';
 import { useNavigate } from 'react-router-dom';
 
 export const ErrorPage = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const redirectToHomePage = () => {
-    navigate('/');
-  };
+	const redirectToHomePage = () => {
+		navigate('/');
+	};
 
-  return (
-    <div className="error-container">
-      <span role="img" aria-label="banner" className="error-logo" />
-      <div className="error-text">
-        <h2 id="error-heading">404</h2>
-        <p>This is not the page you are looking for.</p>
-        <p>Please, go back to the home page.</p>
-      </div>
-      <button className="error-button" onClick={redirectToHomePage}>
-        Back to Home Page
-      </button>
-    </div>
-  );
+	return (
+		<div className="error-container">
+			<span role="img" aria-label="logo-cream-jar" className="error-logo" />
+			<div className="error-text">
+				<h2 id="error-heading">404</h2>
+				<p>This is not the page you are looking for.</p>
+				<p>Please, go back to the home page.</p>
+			</div>
+			<button className="error-button" onClick={redirectToHomePage}>
+				Back to Home Page
+			</button>
+		</div>
+	);
 };
